91fdfe01f0ee0a9897d1fd6ff1225304aead6fc2
Zack S <zechariahs@gmail.com>
2025-12-20 16:04:03 +0000

    Code cleanup and documentation.

--- 2025/day3.js
+++ 2025/day3.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2025 Zechariah Schwenk, All Rights Reserved.
+ * Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://opensource.org/licenses/MIT
+ */
+
+
+/**
+ * 
+ * Processes a list of battery banks to determine the maximum possible "joltage."
+ * 
+ * @param {*} lstInputData Battery banks represented as strings of digits.
+ * @param {*} numMaxBatteries Number of batteries that must be enabled from each bank.
+ * @param {*} debug Whether to output debug information.
+ * @returns {number} Total joltage calculated from the selected batteries from all banks.
+ */
 function processData(lstInputData, numMaxBatteries=2, debug=false) {
 
     let numTotalJolts = 0;
@@ -5,18 +21,11 @@ function processData(lstInputData, numMaxBatteries=2, debug=false) {
     // Iterate through each bank.
     lstInputData.forEach(strBank => {
         
-        let lstEnabledBatteries = [];
-        let lstSelectedIndices = [];
         let lstOriginalBank = strBank.split('').map(num => parseInt(num));        
-        let lstTrackingBank = lstOriginalBank.map(num => num);
-        let numSelectedNumIndex = 0;
-
+        
         if (debug) console.log(`\nProcessing Bank:\t${strBank} `);
 
-        // const digits = bank.split('');
         const numTotalAvailableBatteries = lstOriginalBank.length;
-        // const neededDigits = 12;
-    
         let result = '';
         let currentInputIndex = 0;
     
@@ -52,126 +61,6 @@ function processData(lstInputData, numMaxBatteries=2, debug=false) {
         numTotalJolts += parseInt(result);
 
     });
-
-    /*
-    lstInputData.forEach(strBank => {
-        
-        let lstEnabledBatteries = [];
-        let lstSelectedIndices = [];
-        let lstOriginalBank = strBank.split('').map(num => parseInt(num));        
-        let lstTrackingBank = lstOriginalBank.map(num => num);
-        let lstTrackingBank2 = lstOriginalBank.map(num => num);
-        let numSelectedNumIndex = 0;
-
-        let numMax = 1;
-        let numCurrMax = 0;
-
-        let strBankForDebug = '';
-        lstOriginalBank.forEach((num, idx) => {
-            strBankForDebug += `${num} `.padStart(3, ' ');
-        });
-
-        // if (debug) console.log(`\nProcessing Bank:\t${strBank} `);
-        if (debug) console.log(`\nProcessing Bank:         ${strBankForDebug}`);
-
-        let strIndicesForDebug = '';
-        lstOriginalBank.forEach((num, idx) => {
-            strIndicesForDebug += `${idx} `.padStart(3, ' ');
-        });
-        if (debug) console.log(`\tIndices:         ${strIndicesForDebug}`);
-        
-        for(let numLoopCount = 0; numLoopCount < numMaxBatteries && lstSelectedIndices.length <= numMaxBatteries; numLoopCount++) {
-            
-            // if (numBattery > 0) {
-            //     lstTrackingBank[numSelectedNumIndex] = 0;
-            // }
-            
-            // if (debug) console.log(`\tOriginal Bank: ${lstOriginalBank}`);
-            // if (debug) console.log(`\tTracking Bank: ${lstTrackingBank}`);       
-
-            lstOriginalBank.forEach((num, idx) => {
-                
-                if(!lstSelectedIndices.includes(idx)) {
-                    
-                    // Not used, should it be selected?
-
-                    // Look at the remaining numbers in the tracking bank.
-                    lstOriginalBank.forEach((num2, idx2) => {
-                        
-                        if(!lstSelectedIndices.includes(idx2) && idx2 !== idx) {
-                            if(num2 > num) {
-                                if(lstOriginalBank.length - idx > numMaxBatteries - lstSelectedIndices.length) {
-                                    // Can skip value @ idx
-                                    if(debug) console.log(`\t\tIdx ${idx} (${num}) can be skipped for ${idx2} (${num2})`);
-                                    break;
-                                } else {
-                                    // Must select value @ idx
-                                    lstSelectedIndices.push(idx);
-                                }
-                            }                        
-                        }
-                        
-                        
-                    });
-                            
-
-
-                } else {
-                    // Nothing, already used.
-                }
-            
-            });
-            
-            
-            // let num = lstOriginalBank[numLoopCount];
-
-            
-
-            //     if(num < lstTrackingBank[0] && lstOriginalBank.length - numLoopCount - 1 > numMaxBatteries - lstSelectedIndices.length) {
-            //         // skip
-            //     } else {
-            //         lstSelectedIndices.push(numLoopCount);
-            //     }
-
-            
-            
-            if(numMax < 10) {
-                numMax++;            
-            }
-            
-
-            if(debug) {
-                let strSelectedForDebug = '';
-
-                lstOriginalBank.forEach((num, idx) => {
-                    if(lstSelectedIndices.includes(idx)) {
-                        strSelectedForDebug += `X`.padStart(3, ' ');
-                    } else {
-                        strSelectedForDebug += ` `.padStart(3, ' ');
-                    }
-                });
-
-                if (debug) console.log(`\t${numLoopCount.toString().padStart(3, ' ')}. Selected:  ${strSelectedForDebug}; ${numMax + 1}; (${lstSelectedIndices.length})`);
-            }
-            
-            
-        }
-        
-        // Numeric order.
-        lstSelectedIndices.sort((a, b) => a - b);
-
-        if(debug) console.log(`\tSelected Indices: ${lstSelectedIndices}`);
-
-        lstSelectedIndices.forEach(numIndex => {
-            lstEnabledBatteries.push(lstOriginalBank[numIndex]);            
-        });
-
-        numTotalJolts += parseInt(lstEnabledBatteries.join(''));
-        if (debug) console.log(`\tEnabled Batteries: ${lstEnabledBatteries}, Joined: ${lstEnabledBatteries.join()}, Jolts: ${numTotalJolts}\n`);        
-
-    });
-    */
-
 
     return numTotalJolts;
 
